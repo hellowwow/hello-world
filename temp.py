2cf6d130f072838982900b0b04448a8e6254fab2
liuzhengyu <liuzhengyu@buaa.edu.cn>
2020-11-23 20:32:10 +0800

    temp

---
+++ temp.py
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public int get_index(int a){
+    int [][]s = this.s;
+    int len = s.length - 1;
+    for(int i = 0; i < len; i++){
+        if(s[i] == a) return i;
+    }
+    return -1;
+}

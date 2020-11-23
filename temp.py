5292bc2fdc9153dc995c0539b3ac0dc8c221de17
liuzhengyu <liuzhengyu@buaa.edu.cn>
2020-11-23 20:33:59 +0800

    fix get_index()

--- temp.py
+++ temp.py
@@ -25,7 +25,7 @@
 public int get_index(int a){
     int [][]s = this.s;
     int len = s.length - 1;
-    for(int i = 0; i < len; i++){
+    for(int i = 0; i <= len; i++){
         if(s[i] == a) return i;
     }
     return -1;

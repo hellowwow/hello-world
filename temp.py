c911a678e6aaa49eed07f3b34eab3a18b0f9a17c
liuzhengyu <liuzhengyu@buaa.edu.cn>
2020-11-30 11:29:01 +0800

    Add get_index()

--- temp.py
+++ temp.py
@@ -22,30 +22,3 @@
 
 
 
-public int get_index(int a){
-    int [][]s = this.s;
-    int len = s.length - 1;
-    for(int i = 0; i <= len; i++){
-        if(s[i] == a) return i;
-    }
-    return -1;
-}
-
-
-
-
-
-
-
-
-
-
-
-public int get_index(int a){
-    int [][]s = this.s;
-    int len = s.length - 1;
-    for(int i = 0; i < len; i++){
-        if(s[i] == a) return i;
-    }
-    return -1;
-}
